2fe539faac2e46af07815285d4e23c5056c93730
wilsone_le <wilsone_le@nfcguest.com>
2022-01-21 11:13:39 +0700

    update safe mode mysql

--- db/migrations/007_add_permission_use.sql
+++ db/migrations/007_add_permission_use.sql
@@ -3,5 +3,5 @@
 ALTER TABLE `user` 
 ADD COLUMN `permission` int(2) NOT NULL DEFAULT 1 AFTER `role_label`;
 
-update user set `permission` = 3 where `role` = "staff";
-update user set `permission` = 7 where `role` in ("super_admin", "admin");
+update user set `permission` = 3 where id > 0 and `role` = "staff";
+update user set `permission` = 7 where id > 0 and  `role` in ("super_admin", "admin");
